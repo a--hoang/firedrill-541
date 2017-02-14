8cbd061b2e7c86d55735bf09083253753eca2f9b
Andrew Hoang <ahoang18@bu.edu>
2017-02-14 14:36:48 -0500

    Question 3 done

--- HW2/q2.ml
+++ HW2/q2.ml
@@ -6,7 +6,9 @@ let print_int_ref_array a =
 ;;
 
 let print_float_array a =
-  Array.iter a (fun r -> print_float r; print_char ' ')
+  Array.iter a (fun r -> print_float r; print_char ',');
+  print_char ';';
+  print_newline ()
 ;;
 
 (* prints 2d matrix of float *)
@@ -70,62 +72,55 @@ let q2_calculate p q trials =
     Pervasives.incr table.(!current);
     current := !index;
     index := 0;
+      print_float_array (row_percentages table);
   )
   done;
-  table
 ;;
 
-(* let q3_matrix a d1 d2 =
-let transition = Array.make_matrix 4 4 0.0 in
-  transition.(0).(0) <- (1.0 -. a);
-  transition.(0).(1) <- (1.0 -. p);
-  transition.(0).(2) <- p;
-  transition.(0).(2) <- p;
-  transition.(1).(0) <- 1.0;
-  transition.(1).(1) <- 0.0;
-  transition.(1).(2) <- 1.0;
-  transition.(1).(2) <- 1.0;
-  transition.(2).(0) <- 0.0;
-  transition.(2).(1) <- q;
-  transition.(2).(2) <- (1.0 -. q);
-  transition.(2).(2) <- (1.0 -. q);
-  transition.(2).(0) <- 0.0;
-  transition.(2).(1) <- q;
-  transition.(2).(2) <- (1.0 -. q);
-  transition.(2).(2) <- (1.0 -. q);
-transition
-;; *)
+let q3_matrix a d1 d2 =
+  let transition = Array.make_matrix 4 4 0.0 in
+    transition.(0).(0) <- (1.0 -. a);
+    transition.(0).(1) <- a;
+    transition.(0).(2) <- 0.0;
+    transition.(0).(3) <- 0.0;
+    transition.(1).(0) <- 0.0;
+    transition.(1).(1) <- (1.0 -. d1);
+    transition.(1).(2) <- d1;
+    transition.(1).(3) <- 0.0;
+    transition.(2).(0) <- ((1.0 -. a) *. d2);
+    transition.(2).(1) <- (a *. d2);
+    transition.(2).(2) <- ((1.0 -. a) *. (1.0 -. d2));
+    transition.(2).(3) <- (a *. (1.0 -. d2));
+    transition.(3).(0) <- 0.0;
+    transition.(3).(1) <- d2;
+    transition.(3).(2) <- 0.0;
+    transition.(3).(3) <- (1.0 -. d2);
+  transition
+;;
+
+let q3_calculate a d1 d2 trials =
+  Random.self_init ();
+  let transition = (q3_matrix a d1 d2) in
+  let completions = ref 0 in
+  let current = ref 0 in
+  for i = 1 to trials do (
+    let r = (Random.float 1.0) in
+    let index = ref 0 in
+    while (sum_row transition !current !index) < r do
+      (Pervasives.incr index)
+    done;
+    if (!current == 2 && !index == 0) || (!current == 3 && !index == 1) then
+      Pervasives.incr completions
+    ;
+    current := !index;
+    index := 0;
+    print_float (float_of_int !completions /. float_of_int trials);
+    print_newline ();
+  )
+  done;
+;;
 
 (* 'main' function *)
 let () =
-  print_endline "(𝑝, 𝑞) = (0.5,0.5): ";
-  let t = q2_calculate 0.5 0.5 2000 in
-  (* print_int_ref_array (t5); *)
-  print_float_array (row_percentages t);
-  print_newline ();
-
-  print_endline "(𝑝, 𝑞) = (0.1,0.1): ";
-  let t = q2_calculate 0.1 0.1 2000 in
-  (* print_int_ref_array (t5); *)
-  print_float_array (row_percentages t);
-  print_newline ();
-
-  print_endline "(𝑝, 𝑞) = (0.9,0.9): ";
-  let t = q2_calculate 0.9 0.9 2000 in
-  (* print_int_ref_array (t5); *)
-  print_float_array (row_percentages t);
-  print_newline ();
-
-  print_endline "(𝑝, 𝑞) = (0.1,0.9): ";
-  let t = q2_calculate 0.1 0.9 2000 in
-  (* print_int_ref_array (t5); *)
-  print_float_array (row_percentages t);
-  print_newline ();
-
-  print_endline "(𝑝, 𝑞) = (0.9,0.1): ";
-  let t = q2_calculate 0.9 0.1 2000 in
-  (* print_int_ref_array (t5); *)
-  print_float_array (row_percentages t);
-  print_newline ();
-
+  q3_calculate 0.5 0.5 0.5 2000;
 ;;
